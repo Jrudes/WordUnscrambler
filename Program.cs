5feb8acf7f7306f8e9d9e58c575b3fcb7851b9a5
Jeffrey Rudes <rudesjeff@yahoo.com>
2022-03-30 11:04:30 +0200

    Base program built while taking the course. Initial skeletal code done.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,49 @@ namespace WordUnscrambler
     {
         static void Main(string[] args)
         {
+            bool continueUnscramble = true;
+            do
+            {
+
+                Console.WriteLine("Please choose an option: 'F' to read a File or 'M' for Manual entry.");
+                var option = Console.ReadLine() ?? String.Empty;
+
+                switch (option.ToUpper())
+                {
+
+                    case "F":
+                        Console.Write("Enter file name: ");
+                        ExecuteFileScenario();
+                        break;
+                    case "M":
+                        ExcecuteManualScenario();
+                        Console.Write("Enter words seperated with a comma: ");
+                        break;
+                    default:
+                        Console.WriteLine("Chosen option was not recognized.");
+                        break;
+
+                }
+
+                var continueUnscrambleDecision = string.Empty;
+                do
+                {
+                    Console.WriteLine("Do you want to continue? Y/N");
+                    continueUnscrambleDecision = (Console.ReadLine() ?? string.Empty);
+                } while (!continueUnscrambleDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) &&
+                         !continueUnscrambleDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
+
+                continueUnscramble = continueUnscrambleDecision.Equals("Y", StringComparison.OrdinalIgnoreCase);
+
+            } while (continueUnscramble);
+        }
+
+        private static void ExcecuteManualScenario()
+        {
+        }
+
+        private static void ExecuteFileScenario()
+        {
         }
     }
 }
